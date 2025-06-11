3312c84aa811e2897071e0ccf4ccec3b83f56578
Ansh Koshti <koshtiwork26@gmail.com>
2025-06-11 17:42:29 +0530

    mob script completed.

--- scripts/main.gd
+++ scripts/main.gd
@@ -3,6 +3,9 @@ extends Node
 @export var mob_scene: PackedScene
 var score
 
+func _ready() -> void:
+	pass
+
 func game_over() -> void:
 	$ScoreTimer.stop()
 	$MobTimer.stop()
@@ -13,7 +16,21 @@ func new_game() -> void:
 	$StartTimer.start()
 
 func _on_mob_timer_timeout() -> void:
-	pass # Replace with function body.
+	var mob = mob_scene.instantiate()
+	
+	var mob_spawn_location = $MobPath/MobSpawnLocation
+	mob_spawn_location.progress_ratio = randf()
+	
+	mob.position = mob_spawn_location.position
+	
+	var direction = mob_spawn_location.rotation + PI / 2
+	direction += randf_range(-PI / 4, PI / 4)
+	mob.rotation = direction
+	
+	var velocity = Vector2(randf_range(150.0, 250.0), 0.0)
+	mob.linear_velocity = velocity.rotated(direction)
+	
+	add_child(mob)
 
 func _on_score_timer_timeout() -> void:
 	score += 1
