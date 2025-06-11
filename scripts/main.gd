6de93494fcd206de63bfb6ab9b3333b0742f02f4
Ansh Koshti <koshtiwork26@gmail.com>
2025-06-11 17:26:47 +0530

    2 signals setted

--- scripts/main.gd
+++ scripts/main.gd
@@ -2,3 +2,22 @@ extends Node
 
 @export var mob_scene: PackedScene
 var score
+
+func game_over() -> void:
+	$ScoreTimer.stop()
+	$MobTimer.stop()
+	
+func new_game() -> void:
+	score = 0
+	$Player.start($StartPosition.position)
+	$StartTimer.start()
+
+func _on_mob_timer_timeout() -> void:
+	pass # Replace with function body.
+
+func _on_score_timer_timeout() -> void:
+	score += 1
+
+func _on_start_timer_timeout() -> void:
+	$MobTimer.start()
+	$ScoreTimer.start()
